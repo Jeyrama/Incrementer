90f6d05e3c025772a901de2708c7e0d341f96831
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-18 19:40:36 -0700

    Update Incrementer.js

--- Incrementer.js
+++ Incrementer.js
@@ -13,7 +13,6 @@ Notes:
   arrays will only contain numbers so don't worry about checking that.
 
 Examples:
-
   [1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
   [4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]
 */
